c3b28fe6129f9badabe6cccf75cc7df8025aa9b4
Pooja Rawat <pooja.rawat@ukg.com>
2023-10-02 13:11:53 +0530

    Quiz App

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which language is not supported in android?",
+            "Which virtual machine is used by android?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"java","Kotlin","C#","None of the above"},
+            {"Dalvik OS","JVM","Simple VM","None"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "C#",
+            "Dalvik OS"
+    };
+
+}
